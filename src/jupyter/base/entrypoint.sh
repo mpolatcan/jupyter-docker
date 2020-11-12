e5fbebdd05f92cf4439c51089c7c21a9e2a22fb0
Mutlu Polatcan <mutlupolatcan@gmail.com>
2020-11-13 02:22:28 +0300

    hotfix - docker images updated and refactored

--- src/jupyter/base/entrypoint.sh
+++ src/jupyter/base/entrypoint.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 jupyter notebook --ip ${JUPYTER_IP:=0.0.0.0} \
             --port ${JUPYTER_PORT:=8888} \
             --no-browser \
             --allow-root \
-            --NotebookApp.password=\'${JUPYTER_PASSWORD}\'
+            --NotebookApp.token=''
--- src/jupyterlab/base/entrypoint.sh
+++ src/jupyterlab/base/entrypoint.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 jupyter lab --ip ${JUPYTER_IP:=0.0.0.0} \
             --port ${JUPYTER_PORT:=8888} \
             --no-browser \
             --allow-root \
-            --NotebookApp.password=\'${JUPYTER_PASSWORD}\'
+            --NotebookApp.password=''
